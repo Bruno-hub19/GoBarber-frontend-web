27d5b225f6ec4e2b43c12b93b189bd84dc9b8c28
Bruno <brunnofn19@gmail.com>
2020-07-02 09:45:44 -0300

    add register page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
+import Register from './pages/Register';
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-      <SignIn />
+      <Register />
     </>
   );
 };
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,37 +1,37 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import logoSvg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => {
+const Register: React.FC = () => {
   return (
     <Container>
+      <Background />
       <Content>
         <img src={logoSvg} alt="GoBaber logo" />
 
         <form>
-          <h1>Faça seu logon</h1>
-          <Input icon={FiMail} name="email" type="text" placeholder="E-mail" />
+          <h1>Faça seu Cadastro</h1>
+          <Input icon={FiUser} name="name" placeholder="Nome" />
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
           <Input
             icon={FiLock}
             name="password"
             type="password"
             placeholder="Senha"
           />
-          <Button type="submit">Entrar</Button>
-          <a href="/">Esqueci minha senha</a>
+          <Button type="submit">Cadastrar</Button>
         </form>
 
-        <a href="/register">
-          <FiLogIn size={16} /> Criar conta
+        <a href="/login">
+          <FiArrowLeft size={16} /> Voltar para logon
         </a>
       </Content>
-      <Background />
     </Container>
   );
 };
 
-export default SignIn;
+export default Register;
